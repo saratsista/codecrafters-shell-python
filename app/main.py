37202af1730c9c080a86b3735539e0d516327ba9
Sarat Sista <6644398+saratsista@users.noreply.github.com>
2025-02-01 10:06:27 -0800

    exit builtin

--- app/main.py
+++ app/main.py
@@ -7,6 +7,11 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         command = input()
+        command_parts = command.split()
+        if len(command_parts) > 1:
+            if command_parts[0] == 'exit' and command_parts[1] == '0':
+                sys.exit(0)
+
         print(command + ":","command not found") 
 
 
